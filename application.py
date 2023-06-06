95e814c9a8ea6e9cdc0291bafecd009fc762bd56
gardenjun <jeongwonjun0718@gmail.com>
2023-06-06 04:44:14 +0000

    feat: 자잘한 오류 수정

--- application.py
+++ application.py
@@ -1,4 +1,4 @@
-import test
+import inference
 from flask import Flask, request, render_template
 from flask_cors import CORS
 
@@ -19,12 +19,12 @@ def translation():
         text = form_data["text"]
 
         if form_data["button"] == "s2d":
-            answer = test.s2d(text)
+            answer = inference.s2d(text)
             response["result_text"]= answer
             return response
         
         if form_data["button"] == "d2s":
-            answer = test.d2s(text)
+            answer = inference.d2s(text)
             response["result_text"]= answer
             return response
 
